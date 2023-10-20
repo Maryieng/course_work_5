41ed166a9d2e1a97e621e3a0890f32a481a27898
sorokin <isorokin1985@gmail.com>
2023-10-20 23:16:34 +0300

    add methods in class DBManager

--- src/class_DBManager.py
+++ src/class_DBManager.py
@@ -1,4 +1,5 @@
 import psycopg2
+from pprint import pprint
 
 
 class DBManager():
@@ -9,7 +10,6 @@ class DBManager():
         self.create_database()
         self.create_table_companies()
         self.create_table_vacancies()
-
 
     def create_database(self):
         conn = psycopg2.connect(**self.params)
@@ -27,52 +27,121 @@ class DBManager():
         conn = psycopg2.connect(**self.params)
         cur = conn.cursor()
         cur.execute('''CREATE TABLE companies (
-            supplier_id integer PRIMARY KEY,
+            company_id_hh integer PRIMARY KEY,
             company_name varchar(150),
-            contact varchar(150),
-            address varchar(150),
-            phone varchar(150),
-            fax varchar(150),
-            homepage varchar(150)
+            employer_url varchar(150)
         )''')
         conn.commit()
 
         cur.close()
         conn.close()
-
 
     def create_table_vacancies(self):
         conn = psycopg2.connect(**self.params)
         cur = conn.cursor()
         cur.execute('''CREATE TABLE vacancies (
-            supplier_id integer PRIMARY KEY,
-            company_name varchar(150),
-            contact varchar(150),
-            address varchar(150),
-            phone varchar(150),
-            fax varchar(150),
-            homepage varchar(150)
+            vacancy_id_hh integer PRIMARY KEY,
+            company_id_hh integer,
+            vacancy_name varchar(150),
+            data_published varchar(150),
+            salary_average integer,
+            area varchar(150),
+            url varchar(150),
+            requirement varchar(500),
+            experience varchar(150),
+            employment varchar(150),
+            
+            CONSTRAINT fk_hh_vacancies_vacancies FOREIGN KEY(company_id_hh) REFERENCES companies(company_id_hh)
         )''')
         conn.commit()
 
         cur.close()
         conn.close()
 
+    def insert_data_company(self, data):
+        conn = psycopg2.connect(**self.params)
+        cur = conn.cursor()
 
-    def insert_data(self, table, data):
-        pass
+        cur.execute("INSERT INTO companies VALUES (%s, %s, %s)", (int(data["employer"]["id"]), data["employer"]["name"], data["employer"]["alternate_url"]))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+    def insert_data_vacancy(self, vacancies):
+        conn = psycopg2.connect(**self.params)
+        cur = conn.cursor()
+        for vacancy in vacancies:
+            cur.execute("INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                        (vacancy.id,
+                         vacancy.employer_id,
+                         vacancy.name,
+                         vacancy.data_published,
+                         vacancy.salary_average,
+                         vacancy.area,
+                         vacancy.url,
+                         vacancy.requirement,
+                         vacancy.experience,
+                         vacancy.employment))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
 
     def get_companies_and_vacancies_count(self):
-        pass
+        conn = psycopg2.connect(**self.params)
+        cur = conn.cursor()
+        cur.execute('''SELECT company_name, COUNT(*)
+        FROM vacancies
+        JOIN companies USING (company_id_hh)
+        GROUP BY company_name''')
+        pprint(cur.fetchall())
+        cur.close()
+        conn.close()
 
     def get_all_vacancies(self):
-        pass
+        conn = psycopg2.connect(**self.params)
+        cur = conn.cursor()
+        cur.execute('''SELECT company_name, vacancy_name, salary_average, url
+        FROM vacancies
+        JOIN companies USING (company_id_hh)''')
+        pprint(cur.fetchall())
+        cur.close()
+        conn.close()
 
     def get_avg_salary(self):
-        pass
+        conn = psycopg2.connect(**self.params)
+        cur = conn.cursor()
+        cur.execute('''SELECT AVG(salary_average)
+        FROM vacancies
+        WHERE salary_average > 0''')
+        pprint(cur.fetchall())
+        cur.close()
+        conn.close()
 
     def get_vacancies_with_higher_salary(self):
-        pass
+        conn = psycopg2.connect(**self.params)
+        cur = conn.cursor()
+        cur.execute('''SELECT company_name, vacancy_name, salary_average, url
+        FROM vacancies
+        JOIN companies USING (company_id_hh)
+        WHERE salary_average > (SELECT AVG(salary_average)
+        FROM vacancies
+        WHERE salary_average > 0)
+        ORDER BY salary_average DESC''')
+        pprint(cur.fetchall())
+        cur.close()
+        conn.close()
 
-    def get_vacancies_with_keyword(self):
-        pass
+    def get_vacancies_with_keyword(self, word):
+        conn = psycopg2.connect(**self.params)
+        cur = conn.cursor()
+        cur.execute(f'''SELECT company_name, vacancy_name, salary_average, url
+        FROM vacancies
+        JOIN companies USING (company_id_hh)
+        WHERE vacancy_name LIKE '%{word}%' OR vacancy_name LIKE '{word}%' OR vacancy_name LIKE '%{word}';''')
+        pprint(cur.fetchall())
+        cur.close()
+        conn.close()
