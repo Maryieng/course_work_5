b4c4ee0ad519332def782288596da88ec0bfe604
sorokin <isorokin1985@gmail.com>
2023-10-21 01:10:14 +0300

    docstrings

--- main.py
+++ main.py
@@ -9,30 +9,39 @@ def main():
     database_name = 'hh_vacancies'
     file_companies_ids = 'data/companies.json'
 
+    # loading companies from file
     companies = load_companies(file_companies_ids)
 
+    # loading parameters database from database.ini
     params = config()
 
+    # create database
     db_manager = DBManager(database_name, params)
     print(f'База SQL {db_manager.name} создана')
 
+    # create api manager
     hh_api = HH_API()
 
     for company in companies:
-
+        # getting info from company this id
         vacancies_info = hh_api.get_all_vacancies(company['id'])
+
+        # send info about company in database
         db_manager.insert_data_company(vacancies_info[0])
 
+        # creating list of vacancies
         vacancies = []
         for vacancy_info in vacancies_info:
             vacancy = Vacancy.create_vacancy_from_hh(vacancy_info)
             vacancies.append(vacancy)
 
+        # insert vacancies in database
         db_manager.insert_data_vacancy(vacancies)
 
         print(f"Компания {company['name']} - количество вакансий", len(vacancies_info))
     print('Данные по вакансиям выбранных работодателей добавлены в базу данных SQL')
 
+    # choosing method for working with database
     while True:
 
         print('''
